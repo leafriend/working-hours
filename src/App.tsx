8526358c2272837451a73875df7f5073f18cfa9c
leafriend <leafriend@gmail.com>
2019-05-12 21:27:54 +0900

    Change to load logs from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import { Article } from './Article';
 import { Header } from './Header';
 import { Log, LeaveType } from './log/types';
+import { LocalLogsSet } from './storage/local';
+import { LogsSet } from './storage/types';
 
 const YEAR_MONTH = '2019-05';
 
@@ -20,15 +22,20 @@ const LOGS: Log[] = Array(31).fill(EMPTY_LOG);
 LOGS[date(2)] = { leaveType: LeaveType.WORK, startedAt: '09:57', finishedAt: '19:24' };
 LOGS[date(3)] = { leaveType: LeaveType.WORK, startedAt: '09:57' };
 
+const logsSet: LogsSet = new LocalLogsSet();
+
 const App: React.FC = () => {
+  const [yearMonth, setYearMonth] = useState(YEAR_MONTH);
+  const [logs, setLogs] = useState(logsSet.getLogs(yearMonth));
+
   return (
     <div className="App">
       <Header
         yearMonth={YEAR_MONTH}
       />
       <Article
-        logs={LOGS}
-      />
+        logs={logs}
+        />
     </div>
   );
 }
