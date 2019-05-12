cd6b5987b2cfe650ae0ed6d19ab64fe7e6954b7b
leafriend <leafriend@gmail.com>
2019-05-12 17:00:03 +0900

    Fill const LOGS with empty log

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,16 @@ import { Log, LeaveType } from './log/types';
 
 const MONTH = '2019-05';
 
+const EMPTY_LOG: Log = {
+  leaveType: LeaveType.WORK,
+  startedAt: undefined,
+  finishedAt: undefined,
+}
+
 function date(i: number) {
   return i - 1;
 }
-const LOGS: Log[] = Array(31).fill(undefined);
+const LOGS: Log[] = Array(31).fill(EMPTY_LOG);
 LOGS[date(2)] = { leaveType: LeaveType.WORK, startedAt: '09:57', finishedAt: '19:24' };
 LOGS[date(3)] = { leaveType: LeaveType.WORK, startedAt: '09:57' };
 
