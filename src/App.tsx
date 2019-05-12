5f14fbb50b0428b4f6b9de266cd080c9c6f657d2
leafriend <leafriend@gmail.com>
2019-05-12 21:28:09 +0900

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,10 @@ import React, { useState } from 'react';
 import './App.css';
 import { Article } from './Article';
 import { Header } from './Header';
-import { Log, LeaveType } from './log/types';
 import { LocalLogsSet } from './storage/local';
 import { LogsSet } from './storage/types';
 
 const YEAR_MONTH = '2019-05';
-
-const EMPTY_LOG: Log = {
-  leaveType: LeaveType.WORK,
-  startedAt: undefined,
-  finishedAt: undefined,
-}
-
-function date(i: number) {
-  return i - 1;
-}
-const LOGS: Log[] = Array(31).fill(EMPTY_LOG);
-LOGS[date(2)] = { leaveType: LeaveType.WORK, startedAt: '09:57', finishedAt: '19:24' };
-LOGS[date(3)] = { leaveType: LeaveType.WORK, startedAt: '09:57' };
 
 const logsSet: LogsSet = new LocalLogsSet();
 
