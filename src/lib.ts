489719f48484c9f5081b88221c8d1b5ecc9414ec
leafriend <leafriend@gmail.com>
2019-05-12 23:23:00 +0900

    Add converting functions

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,22 @@
 export function lastDateOf(year: number, month: number): number {
   return new Date(year, month, 0).getDate();
 }
+
+export function zerofill(i: number): string {
+  if (i < 10) {
+    return `0${i}`;
+  } else {
+    return i.toString();
+  }
+}
+
+export function convertTimeToMinutes(time: string): number {
+  const [hours, minutes] = time.split(':').map(str => parseInt(str, 10));
+  return hours * 60 + minutes;
+}
+
+export function convertMinutesToTime(minutes: number): string {
+  const hours = Math.floor(minutes / 60);
+  const rest = minutes - (hours * 60);
+  return `${zerofill(hours)}:${zerofill(rest)}`;
+}
